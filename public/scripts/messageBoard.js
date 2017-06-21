7098a7993c9ae1dd6d7c065cda5ccef4a97e395f
BluePawDev <jaruby@me.com>
2017-06-21 15:17:28 -0500

    Commit: Part 1...2?

--- public/scripts/messageBoard.js
+++ public/scripts/messageBoard.js
@@ -1,44 +1,54 @@
-var myApp = angular.module( 'myApp', [] );
+var myApp = angular.module('myApp', []);
 
-myApp.controller( 'MessageBoardController', function( MessageBoardService ){
-  var vm = this;
+myApp.controller('MessageBoardController', function(MessageBoardService) {
+	var vm = this;
 
-  vm.getMessages = function(){
-    console.log( 'in controller, getMessages');
-    MessageBoardService.retrieveMessages().then( function(){
-      vm.messages = MessageBoardService.data;
-      console.log( 'back in controller with:', vm.messages );
-    });
-  }; //end getMessages
+	vm.getMessages = function() {
+		console.log('in controller, getMessages');
+		MessageBoardService.retrieveMessages().then(function() {
+			vm.messages = MessageBoardService.data;
+			console.log('back in controller with:', vm.messages);
+		});
+	}; //end getMessages
 
-  vm.logOut = function(){
-    console.log( 'logging out', vm.name );
-    vm.hasName = false;
-    vm.name = '';
-  };
+	vm.logIn = function() {
+		console.log('in controller logIn');
+		MessageBoardService.sendLogIn();
+	};
 
-  vm.sendMessage = function(){
-    // used to toggle name input
-    if( !vm.hasName ){
-      vm.hasName = true;
-    }
+	vm.register = function() {
+		console.log('in controller register');
+		MessageBoardService.sendRegister();
+	};
 
-    if( vm.body == '' ){
-      alert( 'do NOT spam us with your empty messages!!!' );
-    } // end empty message
-    else{
-      // create object to send
-      var newMessage = {
-        name: vm.name,
-        body: vm.body
-      }; // end newMessage
-      console.log( 'in controller sending:', newMessage );
-      MessageBoardService.newMessage( newMessage ).then( function(){
-        console.log( 'back in controller after post' );
-        vm.getMessages();
-        vm.body = '';
-      });
-    } // end message exxists
-  };
+	vm.logOut = function() {
+		console.log('logging out', vm.name);
+		vm.hasName = false;
+		vm.name = '';
+	};
+
+	vm.sendMessage = function() {
+		// used to toggle name input
+		if (!vm.hasName) {
+			vm.hasName = true;
+		}
+
+		if (vm.body == '') {
+			alert('do NOT spam us with your empty messages!!!');
+		} // end empty message
+		else {
+			// create object to send
+			var newMessage = {
+				name: vm.name,
+				body: vm.body
+			}; // end newMessage
+			console.log('in controller sending:', newMessage);
+			MessageBoardService.newMessage(newMessage).then(function() {
+				console.log('back in controller after post');
+				vm.getMessages();
+				vm.body = '';
+			});
+		} // end message exxists
+	};
 
 });
--- public/scripts/services/messageBoardService.js
+++ public/scripts/services/messageBoardService.js
@@ -1,25 +1,59 @@
-myApp.service( 'MessageBoardService', function( $http ){
-  var sv = this;
+myApp.service('MessageBoardService', function($http) {
+	var sv = this;
 
-  sv.retrieveMessages = function(){
-    console.log( 'in service, retrieveMessages' );
-    return $http({
-      method: 'GET',
-      url: '/messages'
-    }).then( function( response ){
-      console.log( 'in service back from server with:', response );
-      sv.data = response.data;
-    }); // end http
-  }; //end retrieveMessages
+	sv.retrieveMessages = function() {
+		console.log('in service, retrieveMessages');
+		return $http({
+			method: 'GET',
+			url: '/messages'
+		}).then(function(response) {
+			console.log('in service back from server with:', response);
+			sv.data = response.data;
+		}); // end http
+	}; //end retrieveMessages
 
-  sv.newMessage = function( messageObject ){
-    console.log( 'in service sending:', messageObject );
-    return $http({
-      method: 'POST',
-      url: '/messages',
-      data: messageObject
-    }).then( function( response ){
-      console.log( 'back from post with:', response );
-    }); // end http
-  }; // end newMessage
+
+	sv.sendLogIn = function() {
+		console.log('in service sendLogIn');
+		var credentials = {
+			username: 'Boris',
+			password: 'Karkoff'
+		}
+		$http({
+			method: 'POST',
+			url: '/',
+			data: credentials
+		}).then(function(response) {
+			console.log('back from login attempt:', response);
+		});
+	};
+
+	sv.sendRegister = function() {
+		console.log('in service sendRegister');
+		var credentials = {
+			username: 'Natasha',
+			password: 'Fatale'
+		}
+		$http({
+			method: 'POST',
+			url: '/register',
+			data: credentials
+		}).then(function(response) {
+			console.log('back from register attempt:', response);
+		});
+	};
+
+
+
+
+	sv.newMessage = function(messageObject) {
+		console.log('in service sending:', messageObject);
+		return $http({
+			method: 'POST',
+			url: '/messages',
+			data: messageObject
+		}).then(function(response) {
+			console.log('back from post with:', response);
+		}); // end http
+	}; // end newMessage
 }); //end service
